10aea17c18113e0139a08682384ee8854ef7e2bb
Yury Kuchura <kuchura@gmail.com>
2011-11-19 12:10:51 +0300

    Added event types file for strive
    Signed-off-by: Terraneo Federico <fede.tft@hotmail.it>

---
+++ drivers/event_types_strive.h
@@ -0,0 +1,51 @@
+/***************************************************************************
+ *   Copyright (C) 2011 by Terraneo Federico                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   As a special exception, if other files instantiate templates or use   *
+ *   macros or inline functions from this file, or you compile this file   *
+ *   and link it with other works to produce a work based on this file,    *
+ *   this file does not by itself cause the resulting work to be covered   *
+ *   by the GNU General Public License. However the source code for this   *
+ *   file must still be made available in accordance with the GNU General  *
+ *   Public License. This exception does not invalidate any other reasons  *
+ *   why a work based on this file might be covered by the GNU General     *
+ *   Public License.                                                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
+ ***************************************************************************/
+
+#ifndef EVENT_TYPES_MP3V2_H
+#define	EVENT_TYPES_MP3V2_H
+
+#ifdef _BOARD_MP3V2
+
+class EventType
+{
+public:
+    enum E
+    {
+        Default=0, //This is a must on all backends
+        TouchDown=1,
+        TouchUp=2,
+        TouchMove=3,
+        ButtonA=4,
+        ButtonB=5
+    };
+private:
+    EventType();
+};
+
+#endif //_BOARD_MP3V2
+
+#endif //EVENT_TYPES_MP3V2_H
