afde69f9cd6ec2b6a71796faf3fbe14e9d75752b
aarontognoli <aaron.tognoli@mail.polimi.it>
2024-03-10 11:39:18 +0100

    Added Test for single element list
    Signed-off-by: Federico Terraneo <fede.tft@miosix.org>

--- _examples/scrollingTest/scrollingTest.cpp
+++ _examples/scrollingTest/scrollingTest.cpp
@@ -11,7 +11,7 @@ ENTRY()
     shared_ptr<Window> w= make_shared<Window>();
     
     ScrollingList sl=ScrollingList(w.get(),Point(10,10),10,100);
-    Label l1 = Label(w.get(),Point(10,170),100,20,"Select a number");
+    Label l1 = Label(w.get(),Point(10,220),100,20,"Select a number");
     for( int i=0;i<50;i++)
     {
          sl.addItem(to_string(i));
@@ -21,6 +21,18 @@ ENTRY()
         l1.setText("Selected: "+sl.getSelected());
     });
 
+    ScrollingList sl2=ScrollingList(w.get(),Point(120,30),1,50);
+    Label l2 = Label(w.get(),Point(120,5),100,20,"Select your Beatle");
+
+    sl2.addItem("John");
+    sl2.addItem("Paul");
+    sl2.addItem("George");
+    sl2.addItem("Ringo");
+
+    Label l3 = Label(w.get(),Point(120,220),100,20,"");
+    sl2.setCallback([&l3,&sl2](){
+        l3.setText("Selected: "+sl2.getSelected());
+    });
 
     
     InputHandler::instance().registerEventCallback([w](){
