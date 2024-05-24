0cf426827d1ead52ce6627d11ae8eb17c4b50ead
Federico Terraneo <fede.tft@miosix.org>
2024-05-24 18:23:02 +0200

    Update copyright

--- _tools/code_generators/ttfparser.cpp
+++ _tools/code_generators/ttfparser.cpp
@@ -1,5 +1,6 @@
 /***************************************************************************
  *   Copyright (C) 2010, 2011 by Terraneo Federico                         *
+ *   Copyright (C) 2024 by Terraneo Federico and Nicolas Benatti           *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
