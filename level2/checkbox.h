f4334ffbeb3f5ce5731fb25972ee412a808ba07f
Federico Terraneo <fede.tft@miosix.org>
2024-05-24 19:08:25 +0200

    Fix copyright of Aaron contribution

--- level2/checkbox.h
+++ level2/checkbox.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2014 by Terraneo Federico                               *
+ *   Copyright (C) 2024 by Aaron Tognoli                                   *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
