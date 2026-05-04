7b4841d51e226633b1e6db1f5c059813732464dc
Marco Zanzottera <marcozanzottera@e.email>
2026-05-04 12:57:31 +0200

    Fix edge case of mix method of color and add color docs

--- color.h
+++ color.h
@@ -32,34 +32,67 @@
 namespace mxgui {
 
 // Move RGB565Color definition above all uses
+/**
+ * \ingroup pub_iface
+ * An RGB565Color is used to represent colors in the RGB565 format.
+ * It is suggested to work with the Color typedef to keep the program portable
+ * to different color formats.
+ */
 class RGB565Color
 {
 public:
     constexpr RGB565Color() : color(0) {}
+    
+    /**
+     * Construct a color from red, green, blue values
+     * \param r red value (0-255)
+     * \param g green value (0-255)
+     * \param b blue value (0-255)
+     */
     constexpr RGB565Color(unsigned char r, unsigned char g, unsigned char b)
         : color(((r & 0xf8) << 8) | ((g & 0xfc) << 3) | (b >> 3)) {}
 
+    /**
+     * Get the red channel value, upscaled to 8 bits
+     * \return red value (0-255)
+     */
     constexpr unsigned char getR() const
     {
         unsigned char r = (color >> 8) & 0xf8;
         return r | (r >> 5);
     }
+
+    /**
+     * Get the green channel value, upscaled to 8 bits
+     * \return green value (0-255)
+     */
     constexpr unsigned char getG() const
     {
         unsigned char g = (color >> 3) & 0xfc;
         return g | (g >> 6);
     }
+
+    /**
+     * Get the blue channel value, upscaled to 8 bits
+     * \return blue value (0-255)
+     */
     constexpr unsigned char getB() const
     {
         unsigned char b = (color & 0x1f) << 3;
         return b | (b >> 5);
     }
 
+    /** Comparison operators */
     constexpr bool operator==(const RGB565Color& other) const { return color == other.color; }
     constexpr bool operator!=(const RGB565Color& other) const { return color != other.color; }
     
+    /** Explicit conversion to the raw data type */
     constexpr operator unsigned short() const { return color; }
 
+    /**
+     * Creates a color object from the raw data type
+     * \param raw an unsigned short representing the raw color value
+     */
     static constexpr RGB565Color fromRaw(unsigned short raw)
     {
         RGB565Color c;
@@ -67,23 +100,37 @@ public:
         return c;
     }
 
+    /**
+     * Creates a color object from the raw data type of an RGB565 color
+     * \param c an unsigned short representing the raw color value of an RGB565 color
+     */
     static constexpr RGB565Color fromRGB565(unsigned short c) { return fromRaw(c); }
 
     static constexpr RGB565Color black() { return fromRaw(0x0000); }
     static constexpr RGB565Color white() { return fromRaw(0xffff); }
 
+    /**
+     * Linearly interpolate between two colors
+     * \param c1 the first color
+     * \param c2 the second color
+     * \param scale a blend factor (0-255), where 0 is entirely c1, 255 is entirely c2
+     */
     static constexpr RGB565Color mix(RGB565Color c1, RGB565Color c2, unsigned char scale)
     {
         unsigned short s = scale;
+        unsigned short inv = 255 - s;
 
-        unsigned short r = ( (static_cast<unsigned short>(c1.color >> 11) * (256 - s)) +
-                             (static_cast<unsigned short>(c2.color >> 11) * s) ) >> 8;
+        unsigned short r_val = (static_cast<unsigned short>(c1.color >> 11) * inv) +
+                               (static_cast<unsigned short>(c2.color >> 11) * s);
+        unsigned short r = (r_val + 1 + (r_val >> 8)) >> 8;
 
-        unsigned short g = ( (static_cast<unsigned short>((c1.color & 0x07E0) >> 5) * (256 - s)) +
-                             (static_cast<unsigned short>((c2.color & 0x07E0) >> 5) * s) ) >> 8;
+        unsigned short g_val = (static_cast<unsigned short>((c1.color & 0x07E0) >> 5) * inv) +
+                               (static_cast<unsigned short>((c2.color & 0x07E0) >> 5) * s);
+        unsigned short g = (g_val + 1 + (g_val >> 8)) >> 8;
 
-        unsigned short b = ( (static_cast<unsigned short>(c1.color & 0x001F) * (256 - s)) +
-                             (static_cast<unsigned short>(c2.color & 0x001F) * s) ) >> 8;
+        unsigned short b_val = (static_cast<unsigned short>(c1.color & 0x001F) * inv) +
+                               (static_cast<unsigned short>(c2.color & 0x001F) * s);
+        unsigned short b = (b_val + 1 + (b_val >> 8)) >> 8;
 
         return fromRaw( static_cast<unsigned short>((r << 11) | (g << 5) | b) );
     }
@@ -92,22 +139,55 @@ private:
     unsigned short color;
 };
 
+/**
+ * \ingroup pub_iface
+ * A Gray1Color is used to represent colors with 1 bit (black or white).
+ * It is suggested to work with the Color typedef to keep the program portable
+ * to different color formats.
+ */
 class Gray1Color
 {
 public:
     constexpr Gray1Color() : color(0) {}
+    
+    /**
+     * Construct a color from red, green, blue values
+     * \param r red value (0-255)
+     * \param g green value (0-255)
+     * \param b blue value (0-255)
+     */
     constexpr Gray1Color(unsigned char r, unsigned char g, unsigned char b)
         : color(((static_cast<short>(r) + g + b) >= MXGUI_1BPP_THRESHOLD) ? 1 : 0) {}
 
+    /**
+     * Get the red channel value, upscaled to 8 bits
+     * \return red value (0-255)
+     */
     constexpr unsigned char getR() const { return color ? 255 : 0; }
+
+    /**
+     * Get the green channel value, upscaled to 8 bits
+     * \return green value (0-255)
+     */
     constexpr unsigned char getG() const { return color ? 255 : 0; }
+
+    /**
+     * Get the blue channel value, upscaled to 8 bits
+     * \return blue value (0-255)
+     */
     constexpr unsigned char getB() const { return color ? 255 : 0; }
 
+    /** Comparison operators */
     constexpr bool operator==(const Gray1Color& other) const { return color == other.color; }
     constexpr bool operator!=(const Gray1Color& other) const { return color != other.color; }
     
+    /** Explicit conversion to the raw data type */
     constexpr operator unsigned char() const { return color; }
 
+    /**
+     * Creates a color object from the raw data type
+     * \param raw an unsigned char representing the raw color value
+     */
     static constexpr Gray1Color fromRaw(unsigned char raw)
     {
         Gray1Color c;
@@ -115,6 +195,10 @@ public:
         return c;
     }
 
+    /**
+     * Creates a color object from the raw data type of an RGB565 color
+     * \param c an unsigned short representing the raw color value of an RGB565 color
+     */
     static constexpr Gray1Color fromRGB565(unsigned short c)
     {
         RGB565Color col = RGB565Color::fromRaw(c);
@@ -124,7 +208,12 @@ public:
     static constexpr Gray1Color black() { return fromRaw(0); }
     static constexpr Gray1Color white() { return fromRaw(1); }
 
-    // Mix for 1bpp is thresholding
+    /**
+     * Interpolate between two colors. Mix for 1bpp is thresholding.
+     * \param c1 the first color
+     * \param c2 the second color
+     * \param scale a blend factor (0-255), where 0 is entirely c1, 255 is entirely c2
+     */
     static constexpr Gray1Color mix(Gray1Color c1, Gray1Color c2, unsigned char scale)
     {
         if (c1 == c2) return c1;
@@ -138,25 +227,58 @@ private:
     unsigned char color;
 };
 
+/**
+ * \ingroup pub_iface
+ * A Gray4Color is used to represent colors with 4 bits (grayscale).
+ * It is suggested to work with the Color typedef to keep the program portable
+ * to different color formats.
+ */
 class Gray4Color
 {
 public:
     constexpr Gray4Color() : color(0) {}
+    
+    /**
+     * Construct a color from red, green, blue values
+     * \param r red value (0-255)
+     * \param g green value (0-255)
+     * \param b blue value (0-255)
+     */
     constexpr Gray4Color(unsigned char r, unsigned char g, unsigned char b)
         : color(static_cast<unsigned char>(
             (static_cast<unsigned short>(r) * 77u +
              static_cast<unsigned short>(g) * 150u +
              static_cast<unsigned short>(b) * 29u) >> 12)) {}
 
+    /**
+     * Get the red channel value, upscaled to 8 bits
+     * \return red value (0-255)
+     */
     constexpr unsigned char getR() const { return (color << 4) | color; }
+
+    /**
+     * Get the green channel value, upscaled to 8 bits
+     * \return green value (0-255)
+     */
     constexpr unsigned char getG() const { return (color << 4) | color; }
+
+    /**
+     * Get the blue channel value, upscaled to 8 bits
+     * \return blue value (0-255)
+     */
     constexpr unsigned char getB() const { return (color << 4) | color; }
 
+    /** Comparison operators */
     constexpr bool operator==(const Gray4Color& other) const { return color == other.color; }
     constexpr bool operator!=(const Gray4Color& other) const { return color != other.color; }
     
+    /** Explicit conversion to the raw data type */
     constexpr operator unsigned char() const { return color; }
 
+    /**
+     * Creates a color object from the raw data type
+     * \param raw an unsigned char representing the raw color value
+     */
     static constexpr Gray4Color fromRaw(unsigned char raw)
     {
         Gray4Color c;
@@ -164,6 +286,10 @@ public:
         return c;
     }
 
+    /**
+     * Creates a color object from the raw data type of an RGB565 color
+     * \param c an unsigned short representing the raw color value of an RGB565 color
+     */
     static constexpr Gray4Color fromRGB565(unsigned short c)
     {
         RGB565Color col = RGB565Color::fromRaw(c);
@@ -173,34 +299,69 @@ public:
     static constexpr Gray4Color black() { return fromRaw(0x0); }
     static constexpr Gray4Color white() { return fromRaw(0xF); }
 
+    /**
+     * Linearly interpolate between two colors
+     * \param c1 the first color
+     * \param c2 the second color
+     * \param scale a blend factor (0-255), where 0 is entirely c1, 255 is entirely c2
+     */
     static constexpr Gray4Color mix(Gray4Color c1, Gray4Color c2, unsigned char scale)
     {
         unsigned short s = scale;
-        unsigned short val = ( static_cast<unsigned short>(c1.color) * (256 - s) + static_cast<unsigned short>(c2.color) * s ) >> 8;
-        return fromRaw( static_cast<unsigned char>(val & 0x0F) );
+        unsigned short inv = 255 - s;
+        unsigned short val = static_cast<unsigned short>(c1.color) * inv + static_cast<unsigned short>(c2.color) * s;
+        unsigned short res = (val + 1 + (val >> 8)) >> 8;
+        return fromRaw( static_cast<unsigned char>(res & 0x0F) );
     }
 
 private:
     unsigned char color;
 };
 
+/**
+ * \ingroup pub_iface
+ * An RGB332Color is used to represent colors in the RGB332 format.
+ * It is suggested to work with the Color typedef to keep the program portable
+ * to different color formats.
+ */
 class RGB332Color
 {
 public:
     constexpr RGB332Color() : color(0) {}
+    
+    /**
+     * Construct a color from red, green, blue values
+     * \param r red value (0-255)
+     * \param g green value (0-255)
+     * \param b blue value (0-255)
+     */
     constexpr RGB332Color(unsigned char r, unsigned char g, unsigned char b)
         : color((r & 0xe0) | ((g & 0xe0) >> 3) | (b >> 6)) {}
 
+    /**
+     * Get the red channel value, upscaled to 8 bits
+     * \return red value (0-255)
+     */
     constexpr unsigned char getR() const
     {
         unsigned char r = color & 0xe0;
         return r | (r >> 3) | (r >> 6);
     }
+
+    /**
+     * Get the green channel value, upscaled to 8 bits
+     * \return green value (0-255)
+     */
     constexpr unsigned char getG() const
     {
         unsigned char g = (color << 3) & 0xe0;
         return g | (g >> 3) | (g >> 6);
     }
+
+    /**
+     * Get the blue channel value, upscaled to 8 bits
+     * \return blue value (0-255)
+     */
     constexpr unsigned char getB() const
     {
         unsigned char b = (color & 0x03);
@@ -208,11 +369,17 @@ public:
         return b | (b << 4);
     }
 
+    /** Comparison operators */
     constexpr bool operator==(const RGB332Color& other) const { return color == other.color; }
     constexpr bool operator!=(const RGB332Color& other) const { return color != other.color; }
 
+    /** Explicit conversion to the raw data type */
     constexpr operator unsigned char() const { return color; }
 
+    /**
+     * Creates a color object from the raw data type
+     * \param raw an unsigned char representing the raw color value
+     */
     static constexpr RGB332Color fromRaw(unsigned char raw)
     {
         RGB332Color c;
@@ -220,6 +387,10 @@ public:
         return c;
     }
 
+    /**
+     * Creates a color object from the raw data type of an RGB565 color
+     * \param c an unsigned short representing the raw color value of an RGB565 color
+     */
     static constexpr RGB332Color fromRGB565(unsigned short c)
     {
         RGB565Color col = RGB565Color::fromRaw(c);
@@ -229,13 +400,25 @@ public:
     static constexpr RGB332Color black() { return fromRaw(0x00); }
     static constexpr RGB332Color white() { return fromRaw(0xff); }
 
+    /**
+     * Linearly interpolate between two colors
+     * \param c1 the first color
+     * \param c2 the second color
+     * \param scale a blend factor (0-255), where 0 is entirely c1, 255 is entirely c2
+     */
     static constexpr RGB332Color mix(RGB332Color c1, RGB332Color c2, unsigned char scale)
     {
         unsigned short s = scale;
+        unsigned short inv = 255 - s;
 
-        unsigned short r = ( (static_cast<unsigned short>(c1.color & 0xE0) * (256 - s)) + (static_cast<unsigned short>(c2.color & 0xE0) * s) ) >> 8;
-        unsigned short g = ( (static_cast<unsigned short>(c1.color & 0x1C) * (256 - s)) + (static_cast<unsigned short>(c2.color & 0x1C) * s) ) >> 8;
-        unsigned short b = ( (static_cast<unsigned short>(c1.color & 0x03) * (256 - s)) + (static_cast<unsigned short>(c2.color & 0x03) * s) ) >> 8;
+        unsigned short r_val = (static_cast<unsigned short>(c1.color & 0xE0) * inv) + (static_cast<unsigned short>(c2.color & 0xE0) * s);
+        unsigned short r = (r_val + 1 + (r_val >> 8)) >> 8;
+
+        unsigned short g_val = (static_cast<unsigned short>(c1.color & 0x1C) * inv) + (static_cast<unsigned short>(c2.color & 0x1C) * s);
+        unsigned short g = (g_val + 1 + (g_val >> 8)) >> 8;
+
+        unsigned short b_val = (static_cast<unsigned short>(c1.color & 0x03) * inv) + (static_cast<unsigned short>(c2.color & 0x03) * s);
+        unsigned short b = (b_val + 1 + (b_val >> 8)) >> 8;
 
         return fromRaw( static_cast<unsigned char>((r & 0xE0) | (g & 0x1C) | (b & 0x03)) );
     }
@@ -244,23 +427,58 @@ private:
     unsigned char color;
 };
 
+/**
+ * \ingroup pub_iface
+ * An RGB888Color is used to represent colors in the RGB888 format.
+ * For now, as there are no drivers using this format,
+ * this can be used to have a program perform computations on 24-bit color depth,
+ * and then construct on the fly the correct Color type to interface with the display,
+ * decoupling the program color precision from the display pixel format.
+ */
 class RGB888Color
 {
 public:
     constexpr RGB888Color() : color(0) {}
+    
+    /**
+     * Construct a color from red, green, blue values
+     * \param r red value (0-255)
+     * \param g green value (0-255)
+     * \param b blue value (0-255)
+     */
     constexpr RGB888Color(unsigned char r, unsigned char g, unsigned char b)
         : color((static_cast<unsigned int>(r) << 16) |
                 (static_cast<unsigned int>(g) << 8) | b) {}
 
+    /**
+     * Get the red channel value, upscaled to 8 bits
+     * \return red value (0-255)
+     */
     constexpr unsigned char getR() const { return (color >> 16) & 0xFF; }
+
+    /**
+     * Get the green channel value, upscaled to 8 bits
+     * \return green value (0-255)
+     */
     constexpr unsigned char getG() const { return (color >> 8) & 0xFF; }
+
+    /**
+     * Get the blue channel value, upscaled to 8 bits
+     * \return blue value (0-255)
+     */
     constexpr unsigned char getB() const { return color & 0xFF; }
 
+    /** Comparison operators */
     constexpr bool operator==(const RGB888Color& other) const { return color == other.color; }
     constexpr bool operator!=(const RGB888Color& other) const { return color != other.color; }
 
+    /** Explicit conversion to the raw data type */
     constexpr operator unsigned int() const { return color; }
 
+    /**
+     * Creates a color object from the raw data type
+     * \param raw an unsigned int representing the raw color value
+     */
     static constexpr RGB888Color fromRaw(unsigned int raw)
     {
         RGB888Color c;
@@ -268,6 +486,10 @@ public:
         return c;
     }
 
+    /**
+     * Creates a color object from the raw data type of an RGB565 color
+     * \param c an unsigned short representing the raw color value of an RGB565 color
+     */
     static constexpr RGB888Color fromRGB565(unsigned short c)
     {
         RGB565Color col = RGB565Color::fromRaw(c);
@@ -277,13 +499,26 @@ public:
     static constexpr RGB888Color black() { return fromRaw(0x000000); }
     static constexpr RGB888Color white() { return fromRaw(0xFFFFFF); }
 
+    /**
+     * Linearly interpolate between two colors
+     * \param c1 the first color
+     * \param c2 the second color
+     * \param scale a blend factor (0-255), where 0 is entirely c1, 255 is entirely c2
+     */
     static constexpr RGB888Color mix(RGB888Color c1, RGB888Color c2, unsigned char scale)
     {
         unsigned int v1 = static_cast<unsigned int>(c1), v2 = static_cast<unsigned int>(c2);
-        unsigned short s = scale, inv = 256 - s;
-        unsigned char r = (((v1 >> 16) & 0xFF) * inv + ((v2 >> 16) & 0xFF) * s) >> 8;
-        unsigned char g = (((v1 >>  8) & 0xFF) * inv + ((v2 >>  8) & 0xFF) * s) >> 8;
-        unsigned char b = (( v1        & 0xFF) * inv + ( v2        & 0xFF) * s) >> 8;
+        unsigned short s = scale, inv = 255 - s;
+        
+        unsigned int r_val = ((v1 >> 16) & 0xFF) * inv + ((v2 >> 16) & 0xFF) * s;
+        unsigned char r = (r_val + 1 + (r_val >> 8)) >> 8;
+        
+        unsigned int g_val = ((v1 >>  8) & 0xFF) * inv + ((v2 >>  8) & 0xFF) * s;
+        unsigned char g = (g_val + 1 + (g_val >> 8)) >> 8;
+        
+        unsigned int b_val = (v1 & 0xFF) * inv + (v2 & 0xFF) * s;
+        unsigned char b = (b_val + 1 + (b_val >> 8)) >> 8;
+        
         return RGB888Color(r, g, b);
     }
 
